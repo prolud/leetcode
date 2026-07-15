fb1e5b46f88f718dd059f64b731831c1cb0b5614
Lucas Aguiar <7lucasdaniel@gmail.com>
2026-07-14 23:36:39 -0300

    greatest-common-divisor-of-strings

---
+++ greatest-common-divisor-of-strings.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/greatest-common-divisor-of-strings
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ""
+        from math import gcd
+        return str1[:gcd(len(str1), len(str2))]
+    
+Solution.gcdOfStrings(Solution, "ABABAB", "ABAB")
